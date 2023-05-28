a7d35b450d2e048437e5423a2f4402d27e4b39e7
Baeyung <ahmadsheikh617@gmail.com>
2023-05-29 00:00:37 +0500

    different ways to use the selectors

--- intro-and-basics/src/app/servers/servers.component.ts
+++ intro-and-basics/src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //there are different ways we can use the selectors, see commented usages in the appcomponent.html file
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.scss']
 })
