6681654f1434632c923378b8059b56603fcdd8c1
Baeyung <ahmadsheikh617@gmail.com>
2023-05-28 23:52:41 +0500

    styling the array

--- intro-and-basics/src/app/app.component.ts
+++ intro-and-basics/src/app/app.component.ts
@@ -3,7 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'] // we can either have a file or we can also define the styles in the component example below
+  // styles or stylesURL are array as they can take multiple refrences and the one define later overrides if any styles are same
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: red;
+  //     }
+  //   `,
+  //   `
+  //     h1 {
+  //       color: yellow;
+  //     }
+  //   `
+  // ]
 })
 export class AppComponent {
   title = 'intro-and-basics';
