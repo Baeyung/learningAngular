9786b9ab12df75db2a90366a9d9504e264fbe6ac
Baeyung <ahmadsheikh617@gmail.com>
2023-05-29 22:34:32 +0500

    changing the structure according to maximillain

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecepieListComponent } from './recepie-book/recepie-list/recepie-list.component';
-import { RecepieItemComponent } from './recepie-book/recepie-item/recepie-item.component';
-import { RecepieDetailComponent } from './recepie-book/recepie-detail/recepie-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-edit/shopping-list-edit.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HeaderComponent } from './header/header.component';
     RecepieItemComponent,
     RecepieDetailComponent,
     HeaderComponent,
+    RecepiesComponent,
   ],
   imports: [
     BrowserModule,
